53bd269c7f2b5b6b929f4a5d0848530cd0743b3a
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-01-12 22:36:49 -0500

    create button component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,16 @@
 var React = require('react');
 
-module.exports = React.createClass({
+var buttonStyle = {
+  margin: '10px 10px 10px 0'
+};
+
+var Button = React.createClass({
   render: function() {
-    return null;
+    return(
+      <button
+        className="btn btn-default"
+        style={buttonStyle}
+        onClick={this.props.handleClick}>{this.props.label}</button>
+    );
   }
 });
